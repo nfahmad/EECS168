6b8d23861a6e7915120ab7f50b24003d0936e04d
Nabeel Ahmad <143845214+nfahmad@users.noreply.github.com>
2024-03-08 00:10:36 -0600

    Added files relevant to Lab1

---
+++ Lab1/lab01-exercise01.py
@@ -0,0 +1,12 @@
+'''
+Author: Nabeel Ahmad
+KUID: 3096518
+Date: 09/02/2022
+Lab: lab1
+Last modified: 09/02/2022
+Purpose: This is a python program that prints the output on your screen.
+'''
+
+
+
+print("This is my first lab exercise!")
---
+++ Lab1/lab01-exercise02.py
@@ -0,0 +1,20 @@
+'''
+Author: Nabeel Ahmad
+KUID: 3096518
+Date: 09/02/2022
+Lab: lab1
+Last modified: 09/02/2022
+Purpose: this file displays my name, major, and hobbies.
+'''
+
+
+
+print("My name is Nabeel Ahmad.")
+print("I am an EECS major.")
+print("My hobbies are:")
+print("\tListening to music")
+print("\tPlaying video games")
+print("\tBuilding computers")
+print("\tReading books")
+print("Goodbye")
+ 
